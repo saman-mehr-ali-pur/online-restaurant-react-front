476a856d495ff834837d064ce8834f39b28f3c5d
Saman <saman_mehr_ali_poor@tutanota.com>
2024-04-27 17:40:20 +0330

    menu page added

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom/client'
 // import App from './App.jsx'
 // import './index.css'
 import Home from './pages/Home.jsx'
+import Menu from "./pages/Menu.jsx"
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home/>
+    {/* <Home/> */}
+    <Menu/>
   </React.StrictMode>,
 )
--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,11 +1,31 @@
+import Navbar from "../component/NavBar"
 
-
+import "../css/menu/menuStyle.css"
+import foodicon from "../assets/foodicon.svg"
 const Menu = () => {
 
 
-
+    let data = [];
+    for (let i=0;i<28;i++){
+        data.push(i)
+    }
     return <>
-    
+    <Navbar/>
+    <div id="ctn" className="container" >
+        <div className="right-side">
+            {data.map((item,index) => {return <div key={index} className="item-c">
+                <div className="pic-c"><img src={foodicon} alt="food" /></div>
+                <div className="des-c"><p>item</p></div>
+            </div>})}
+        </div>
+        <div className="left-side">
+            <div className="item"><p>foods</p></div>
+            <div className="item"><p>drinks</p></div>
+            <div className="item"><p>desserts</p></div>
+        </div>
+    </div>
     
     </>
 }
+
+export default Menu;
